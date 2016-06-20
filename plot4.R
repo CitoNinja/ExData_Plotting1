81adb30b3c0280913dfd86cda081a294b9bbc8eb
CitoNinja <CitoNinja@users.noreply.github.com>
2016-06-19 22:04:22 -0500

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -1,4 +1,8 @@
 ## using the subset from plot 1 script
+# dataFile <- "./data/household_power_consumption.txt"
+# data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+## creating the subset data
+# subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
 
 ## creating date/time variable
 datetime <- strptime(paste(subSetData$Date, subSetData$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
